f62afd2e62ea914ae2000e5dcec0875a86a80c8e
edimhelena <edimhelenaguimaraes@gmail.com>
2025-07-27 15:27:53 -0300

    fazendo a documentação das rotas de caso

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const PORT = 3000;
 
 const casosRouter = require('./routes/casosRoutes')
 const agentesRouter = require('./routes/agentesRoutes')
+const swaggerUi = require("swagger-ui-express")
 const errorHandler = require('./utils/errorHandler')
 
 app.use(express.json());
 
+const swaggerDocs = require("./docs/swagger.json")
+app.use('/api.docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use('/casos', casosRouter)
 app.use('/agentes', agentesRouter)
 app.use(errorHandler)
